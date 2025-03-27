a389bc1a67f3b36afcb12f3ec302937ab55e2077
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-03-28 01:26:25 +0530

    First commit of booking sevice api

---
+++ src/routes/v1/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.use('/v1',v1ApiRoutes);
+
+
+module.exports = router;
---
+++ src/utils/errors/App-error.js
@@ -0,0 +1,14 @@
+class AppError extends Error{
+    constructor(
+        name,
+        message,
+        explanation,
+        statusCodes
+    ){
+        super();
+        this.name = name;
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCodes = statusCodes;
+    }
+}
